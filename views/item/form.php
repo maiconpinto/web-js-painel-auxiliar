1da6a321f8af333605b8538c3581edb33544ce14
eduardoabach <eduardoabach@gmail.com>
2016-06-22 16:45:49 -0300

    Prototipo item add dinâmico.

---
+++ views/item/form.php
@@ -0,0 +1,13 @@
+<div class="col-lg-12">
+	<div class="panel panel-default">
+        <div class="panel-heading">
+        	<div class="row">
+	        	<div id="item-titulo" class="col-lg-10"></div>
+	        	<div id="item-opt" class="col-lg-2">
+	        		<button type="button" id="btn-item-fechar" class="btn btn-danger">x</button>
+	        	</div>
+        	</div>
+    	</div>
+        <div id="item-content" class="panel-body"></div>
+    </div>
+</div>
---
+++ views/item_teste/form.php
@@ -0,0 +1,17 @@
+<div class="row">
+	<div class="col-xs-3 nopadding"><button type="button" class="btn btn-lg btn-block btn-info btn-test">test</button></div>
+</div>
+<div class="row">
+	<div class="col-xs-3 nopadding">
+		<button type="button" class="btn btn-lg btn-block btn-info btn-num-calc" data-comando="4">4</button>
+	</div>
+	<div class="col-xs-3 nopadding">
+		<button type="button" class="btn btn-lg btn-block btn-info btn-num-calc" data-comando="8">8</button>
+	</div>
+	<div class="col-xs-3 nopadding">
+		<button type="button" class="btn btn-lg btn-block btn-info btn-num-calc" data-comando="9">9</button>
+	</div>
+	<div class="col-xs-3 nopadding">
+		<button type="button" class="btn btn-lg btn-block btn-default btn-num-calc" data-comando="+">+</button>
+	</div>
+</div>
