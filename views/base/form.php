b4330eaeb77121e456b6c041e8827a64565cd882
eduardoabach <eduardoabach@gmail.com>
2016-06-22 16:47:25 -0300

    Suporte a prototipo de item variável.

--- views/base/form.php
+++ views/base/form.php
@@ -4,10 +4,12 @@
 		<button type="button" class="btn painel-option" data-option="calculadora">Calculadora</button>
 		<button type="button" class="btn painel-option" data-option="cores">Cores</button>
 		<button type="button" class="btn painel-option" data-option="busca_painel">Busca</button>
+		<button type="button" class="btn item-painel" data-option="item_teste">Item</button>
 	</div>
 </div>
 <div class="row">
 	<div class="col-lg-12" id="base-content"></div>
+	<div class="col-lg-12" id="item-list"></div>
 </div>
 
 
