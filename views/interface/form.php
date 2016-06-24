77ee143a17a98ce6cfd8eec25b7c04e048ef3599
Eduardo Bach <eduardoabach@gmail.com>
2016-06-24 19:48:25 -0300

    Item test removed

--- views/interface/form.php
+++ views/interface/form.php
@@ -4,7 +4,6 @@
 		<button type="button" class="btn get-module" data-option="calculadora">Calculadora</button>
 		<button type="button" class="btn get-module" data-option="cores">Cores</button>
 		<button type="button" class="btn get-module" data-option="search">Busca</button>
-		<button type="button" class="btn get-module" data-option="item_teste">Item</button>
 		<button type="button" class="btn get-module" data-option="gerador_docs">Gerar Docs</button>
 	</div>
 </div>
