f4aead4db1672ff335f225c8f1725b9ea1ae38fa
eduardoabach <eduardoabach@gmail.com>
2016-06-24 17:44:50 -0300

    Calc init by new type.

--- modules/calculadora.js
+++ modules/calculadora.js
@@ -1,53 +1,58 @@
-var obj_calculadora = {
-	Url: function(view){
-		return url_view('calculadora',view);
-	},
-	Init: function(el_local){
-		var self = this;
-		var url = this.Url('form');
+define(
+	["jquery", "func", "func_number", "bootstrap"],
+	function ($, func, func_number) {
+		return {
+			Url: function(view){
+				return url_view('calculadora',view);
+			},
+			Init: function(el_local){
+				var self = this;
+				var url = this.Url('form');
 
-		$.post(url, {teste: 'casa'}, function (html_view) {
-			el_local.html(html_view);
-			self.Render(el_local);
-		});
-	},
-	Render: function(el_content){
-		var self = this;
-		self.RenderIMC(el_content.find('#estrutura-imc'));
-		self.RenderCalc(el_content.find('#estrutura-calc'));
-	},
-	RenderIMC: function(el_content){
-		var self = this;
-		el_content.find('#btn-calc-imc').click(function(){
-			var peso_imc = el_content.find('#peso-imc').val();
-			var altura_imc = el_content.find('#altura-imc').val();
-			var resultado_imc = peso_imc / (altura_imc * altura_imc);
-			el_content.find('#result-imc').val(resultado_imc);
-		});
-	},
-	RenderCalc: function(el_content){
-		var self = this;
-		el_content.find('.btn-num-calc').click(function(){
-			var comando = $(this).data('comando');
-			self.EventoComandoCalc(comando,el_content);
-		});
+				$.post(url, {teste: 'casa'}, function (html_view) {
+					el_local.html(html_view);
+					self.Render(el_local);
+				});
+			},
+			Render: function(el_content){
+				var self = this;
+				self.RenderIMC(el_content.find('#estrutura-imc'));
+				self.RenderCalc(el_content.find('#estrutura-calc'));
+			},
+			RenderIMC: function(el_content){
+				var self = this;
+				el_content.find('#btn-calc-imc').click(function(){
+					var peso_imc = el_content.find('#peso-imc').val();
+					var altura_imc = el_content.find('#altura-imc').val();
+					var resultado_imc = peso_imc / (altura_imc * altura_imc);
+					el_content.find('#result-imc').val(resultado_imc);
+				});
+			},
+			RenderCalc: function(el_content){
+				var self = this;
+				el_content.find('.btn-num-calc').click(function(){
+					var comando = $(this).data('comando');
+					self.EventoComandoCalc(comando,el_content);
+				});
 
-		el_content.find('#result-calc').keyup(function( event ){
-			if ( event.which == 13 ) {
-				event.preventDefault();
-				self.EventoComandoCalc('=',el_content);
+				el_content.find('#result-calc').keyup(function( event ){
+					if ( event.which == 13 ) {
+						event.preventDefault();
+						self.EventoComandoCalc('=',el_content);
+					}
+				});
+			},
+			EventoComandoCalc: function(comando,el_content){
+				var result_calc = el_content.find('#result-calc');
+				if(comando == 'c'){
+					result_calc.val('');
+				} else if(comando == '='){
+					result_calc.val(eval(result_calc.val()));
+					//Calc.Input.value = eval(Calc.Input.value)
+				} else {
+					result_calc.val(result_calc.val()+comando);
+				}
 			}
-		});
-	},
-	EventoComandoCalc: function(comando,el_content){
-		var result_calc = el_content.find('#result-calc');
-		if(comando == 'c'){
-			result_calc.val('');
-		} else if(comando == '='){
-			result_calc.val(eval(result_calc.val()));
-			//Calc.Input.value = eval(Calc.Input.value)
-		} else {
-			result_calc.val(result_calc.val()+comando);
 		}
 	}
-}
+);
