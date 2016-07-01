42207ee6bfe8d6c66283d9fb52c09cc9a182cb39
eduardoabach <eduardoabach@gmail.com>
2016-07-01 17:38:21 -0300

    Colors panel object js ajust.

--- modules/cores.js
+++ modules/cores.js
@@ -1,18 +1,25 @@
-var obj_cores = {
-	Url: function(view){
-		return url_view('cores',view);
-	},
-	Init: function(el_local){
-		var self = this;
-		var url = this.Url('form');
+define(
+	["jquery", "bootstrap"],
+	function ($) {
+		return {
+			Title: 'Cores',
+			SizeClass: 'col-lg-8',
+			Url: function(view){
+				return url_view('cores',view);
+			},
+			Init: function(el_local){
+				var self = this;
+				var url = this.Url('form');
 
-		$.post(url, {}, function (html_view) {
-			el_local.html(html_view);
-			self.Render(el_local);
-		});
-	},
-	Render: function(el_content){
-		var self = this;
-		//el_content.find('#estrutura');
+				$.post(url, {}, function (html_view) {
+					el_local.html(html_view);
+					self.Render(el_local);
+				});
+			},
+			Render: function(el_content){
+				var self = this;
+				//el_content.find('#estrutura');
+			}
+		}
 	}
-}
+);
