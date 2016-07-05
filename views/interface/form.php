d82a144484a8e1de0870f01a09f31dc52b5952cb
Eduardo Bach <eduardoabach@gmail.com>
2016-07-05 18:49:23 -0300

    module calc ajusts

--- views/interface/form.php
+++ views/interface/form.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div class="col-lg-12">
 		<button type="button" class="btn get-module" data-option="bootstrap_help">Bootstrap</button>
-		<button type="button" class="btn get-module" data-option="calculadora">Calculadora</button>
+		<button type="button" class="btn get-module" data-option="calc">Calculadora</button>
 		<button type="button" class="btn get-module" data-option="cores">Cores</button>
 		<button type="button" class="btn get-module" data-option="search">Busca</button>
 		<button type="button" class="btn get-module" data-option="gerador_docs">Gerar Docs</button>
