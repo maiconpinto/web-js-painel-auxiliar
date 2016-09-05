c4794e4ee76f9e747e575984ef86f92d0f9dd239
eduardoabach <eduardoabach@gmail.com>
2016-09-05 17:22:29 -0300

    Cript desc ajust

--- views/cript/form.php
+++ views/cript/form.php
@@ -1,11 +1,11 @@
 <div class="row">
 	<div class="col-lg-12">
-	    <input class="form-control" id="cript-text" placeholder="Texto para criptografar(base 64)">
+	    <input class="form-control" id="cript-text" placeholder="Texto">
     </div>
 </div>
 <div class="row">
 	<div class="col-lg-12">
-	    <button type="button" class="btn btn-info" id="cript-act-e">Encode</button> 
-	    <button type="button" class="btn btn-info" id="cript-act-d">Decode</button> 
+	    <button type="button" class="btn btn-info" id="cript-act-e">Encode 64</button> 
+	    <button type="button" class="btn btn-info" id="cript-act-d">Decode 64</button> 
     </div>
 </div>
