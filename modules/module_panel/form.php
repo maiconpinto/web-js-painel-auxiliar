46e22f8b968640affda5b3e1b2d0e31e16660e14
eduardoabach <eduardoabach@gmail.com>
2017-02-14 17:31:54 -0200

    Ajust layout interface to panel modulo.

--- modules/module_panel/form.php
+++ modules/module_panel/form.php
@@ -1,13 +1,21 @@
 <div class="col-lg-12 min-padding">
-	<div class="panel panel-default">
+	<div class="panel panel-info">
         <div class="panel-heading">
         	<div class="row">
-	        	<div id="item-titulo" class="col-lg-8"></div>
+	        	<div  id="item-titulo" class="col-lg-8"></div>
 	        	<div id="item-opt" class="col-lg-4">
-	        		<button type="button" id="btn-item-fechar" class="btn pull-right btn-danger btn-xs">x</button>
-	        		<button type="button" id="btn-item-fechar" class="btn pull-right btn-primary btn-xs">+</button>
-	        		<button type="button" id="btn-item-fechar" class="btn pull-right btn-primary btn-xs">-</button>
+	        		<button type="button" id="btn-item-fechar" class="btn pull-right btn-xs">x</button>
+	        		<button type="button" id="btn-item-fechar" class="btn pull-right btn-xs">+</button>
+	        		<button type="button" id="btn-item-fechar" class="btn pull-right btn-xs">-</button>
 	        	</div>
+	        	<!-- <div class="col-lg-12">
+	        		<span id="item-titulo"></span>
+	        		<span id="item-opt">
+		        		<button type="button" id="btn-item-fechar" class="btn pull-right btn-xs">x</button>
+		        		<button type="button" id="btn-item-fechar" class="btn pull-right btn-xs">+</button>
+		        		<button type="button" id="btn-item-fechar" class="btn pull-right btn-xs">-</button>
+	        		</span>
+	        	</div> -->
         	</div>
     	</div>
         <div id="item-content" class="panel-body"></div>
