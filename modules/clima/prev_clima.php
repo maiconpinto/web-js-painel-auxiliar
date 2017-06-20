717fbdd057473ae26d4c79653eebe8c18db5dd79
eduardoabach <eduardoabach@gmail.com>
2017-06-20 14:28:07 -0300

    Tamanho da imagem do clima ajustada

--- modules/clima/prev_clima.php
+++ modules/clima/prev_clima.php
@@ -62,7 +62,7 @@ $xmlOb = new SimpleXMLElement($page);
 								<small><?=$infoUv['risco']?></small>
 							</td>
 							<td align="center">
-								<img width="90%" height="90%" src='modules/clima/img/<?=trim($diaPrev->tempo)?>.png'>
+								<img width="55" height="37" src='modules/clima/img/<?=trim($diaPrev->tempo)?>.png'>
 							</td>
 						</tr>
 						<?php
